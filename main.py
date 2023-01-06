eda93807eb47273b67a2d85890103778427112af
ifkerri <ifkerri@mail.ru>
2023-01-06 17:26:38 +0500

    add: lib

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+import requests as r
 
+ua = UserAgent()
+url = 'https://rustih.ru/'
+headers = {
+    'Accept': '*/*',
+    'User-Agent': ua.random
+}
+
+response = r.get(url=url, headers=headers)
+soup = BeautifulSoup(response.text, 'lxml')
