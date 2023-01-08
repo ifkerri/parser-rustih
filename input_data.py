32583a180cca9bd5a9fb5756e758090c85c33074
ifkerri <ifkerri@mail.ru>
2023-01-08 14:58:41 +0500

    fix: refactoring logic

---
+++ input_data.py
@@ -0,0 +1,7 @@
+# входящие данные для парсинга
+DATA = {
+    'rus_poet_klassik': {
+        'url': 'https://rustih.ru/stihi-russkih-poetov-klassikov/',
+        'part': 'Русские'
+    }
+}
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from fake_useragent import UserAgent
 import requests as r
 import csv
 import json
-import pymorphy2
+import os
+from input_data import DATA
 
 
 def get_author(text):
@@ -11,105 +12,158 @@ def get_author(text):
     author_list = text[:pos].strip().split()
     author_list.reverse()
     author = ' '.join(author_list)
-    # print(author)
     return (author)
 
 
 def get_name(text):
     pos = text.find('—')
-    name = text[pos+1:].strip()
+    name = text[pos + 1:].strip()
     # print(name)
     return (name)
 
 
-# def get_author_gent(author, morph):
-#     parse_author = morph.parse(author)[0]
-#     gent = parse_author.inflect({'gent'})
-#     print(gent.word)
-#     return gent.word
+def get_data(url, folder_name, part):
+    if not os.path.exists('data'):
+        os.mkdir('data')
 
-# morph = pymorphy2.MorphAnalyzer()
-input_data = {
-    'rus_poet_klassik': {
-        'url': 'https://rustih.ru/stihi-russkih-poetov-klassikov/',
-        'part': 'Русские'
+    headers = {
+        'Accept': '*/*',
+        'User-Agent': UserAgent().random
     }
-}
 
-user_agent = UserAgent()
-headers = {
-    'Accept': '*/*',
-    'User-Agent': user_agent.random
-}
+    page_number = 1
+    while url != None:
+        print(f'Обрабатывается страница: {page_number}')
 
-for key, value in input_data.items():
+        page_folder_name = f'data/{folder_name}_page{page_number}'
+        if not os.path.exists(page_folder_name):
+            os.mkdir(page_folder_name)
 
-    response = r.get(url=value.get('url'), headers=headers)
-    src = response.text
-
-    with open(f'data/{key}.html', 'w', encoding='utf-8') as file:
-        file.write(src)
-
-    hrefs = {}
-    soup = BeautifulSoup(src, 'lxml')
-    all_content_block = soup.find_all(class_='entry-title')
-    for content_block in all_content_block:
-        item_href = content_block.findNext('a')
-        text = item_href.text
-        href = item_href.get('href')
-        hrefs[text] = href
-
-    with open(f'data/{key}.json', 'w', encoding='utf-8') as file:
-        json.dump(hrefs, file, indent=4, ensure_ascii=False)
-
-    with open(f'data/{key}.csv', 'w', encoding='utf-8') as file:
-        row = ['Раздел', 'Автор', 'Автор2', 'Название', 'Стих', 'Категории', 'Ссылка']
-        writer = csv.writer(file, delimiter='|')
-        writer.writerow(row)
-
-    count = 0
-    for text, href in hrefs.items():
-        response = r.get(url=href, headers=headers)
+        response = r.get(url, headers=headers)
         src = response.text
+
+        with open(f'{page_folder_name}/page{page_number}.html', 'w', encoding='utf-8') as file:
+            file.write(src)
+
         soup = BeautifulSoup(src, 'lxml')
 
-        author = get_author(text)
-        author2 = author
-        poem_name = get_name(text)
+        # собираем все ссылки на стихотворения
+        poems_on_page = {}
+        contents_on_page = soup.find_all(class_='entry-title')
+        for content_on_page in contents_on_page:
+            item = content_on_page.findNext('a')
+            poems_on_page[item.get('href')] = item.text
 
-        poem_text = []
-        all_part_poem_text = soup.find('div', class_='poem-text').findChildren()
-        for part_poem_text in all_part_poem_text:
-            class_adsbygoogle = part_poem_text.get('class')
-            if class_adsbygoogle == ['adsbygoogle']:
-                break
-            poem_text.append(str(part_poem_text))
-        poem_text_ = ' '.join(poem_text)
+        # запишем информацию в файл JSON
+        with open(f'{page_folder_name}/page{page_number}.json', 'w', encoding='utf-8') as file:
+            json.dump(poems_on_page, file, indent=4, ensure_ascii=False)
 
-        category_list = []
-        all_category = soup.find('ul', class_='post-categories').find_all('a')
-        for category in all_category:
-            category_list.append(category.text)
-        category_text = ','.join(category_list)
-        # print(category_text)
+        # запишем заголовок таблицы CSV
+        with open(f'{page_folder_name}/page{page_number}.csv', 'w', encoding='utf-8-sig') as file:
+            writer = csv.writer(file, delimiter=';')
+            writer.writerow(
+                (
+                    'Раздел',
+                    'Автор',
+                    'Название',
+                    'Ссылка',
+                    'Стихотворение (отрывок)',
+                    'Категории'
+                )
+            )
 
-        poem_href = href
+        # обходим каждую ссылку и собираем информацию о стихотворении
+        # for poem_href, poem_name in poems_on_page.items():
+        #     a=1
 
-        with open(f'data/{key}.csv', 'a', encoding='utf-8') as file:
-            row = [value.get('part'), author, author2, poem_name, poem_text_, category_text, poem_href]
-            writer = csv.writer(file, delimiter='|')
-            writer.writerow(row)
+        # ищем следующую страницу
+        next_page = soup.find('a', class_='next page-numbers')
+        url = None if next_page == None else next_page.get('href')
+        page_number += 1
 
-        # poem_text =
-        # author2 = get_author_gent(author, morph)
-        # break
-        count+=1
-        if count > 1:
+        # аварийный выход из цикла
+        if page_number == 10:
             break
-        # row = [
-        #     value.get('part'),
-        #
-        # ]
-        # with open(f'data/{key}.csv', 'a', encoding='utf-8') as file:
-        #     writer = csv.writer(file)
-        #     writer.writerow(row)
+    # for key, details in input_data.items():
+    #
+    #     response = r.get(details['url'], headers=headers)
+    #     src = response.text
+    #
+    #     with open(f'data/{key}.html', 'w', encoding='utf-8') as file:
+    #         file.write(src)
+    #
+    #     poem_on_page = {}
+    #     soup = BeautifulSoup(src, 'lxml')
+    #     all_content_block = soup.find_all(class_='entry-title')
+    #     for content_block in all_content_block:
+    #         item_href = content_block.findNext('a')
+    #         text = item_href.text
+    #         href = item_href.get('href')
+    #         poem_on_page[text] = href
+    #
+    #     with open(f'data/{key}.json', 'w', encoding='utf-8') as file:
+    #         json.dump(poem_on_page, file, indent=4, ensure_ascii=False)
+
+    # with open(f'data/{key}.csv', 'w', encoding='utf-8-sig') as file:
+    #     row = ['Раздел', 'Автор', 'Автор2', 'Название', 'Стих', 'Категории', 'Ссылка']
+    #     writer = csv.writer(file, delimiter=';')
+    #     writer.writerow(row)
+
+    # count = 0
+    # all_count = len(poem_on_page)
+    # for text, href in poem_on_page.items():
+    #     count += 1
+    #     response = r.get(url=href, headers=headers)
+    #     src = response.text
+    #     soup = BeautifulSoup(src, 'lxml')
+    #
+    #     author = get_author(text)
+    #     author2 = author
+    #     poem_name = get_name(text)
+    #
+    #     poem_text = []
+    #     all_part_poem_text = soup.find('div', class_='poem-text').findChildren()
+    #     for part_poem_text in all_part_poem_text:
+    #         class_adsbygoogle = part_poem_text.get('class')
+    #         if class_adsbygoogle == ['adsbygoogle']:
+    #             break
+    #         poem_text.append(str(part_poem_text))
+    #     poem_text_ = ' '.join(poem_text)
+    #
+    #     category_list = []
+    #     all_category = soup.find('ul', class_='post-categories').find_all('a')
+    #     for category in all_category:
+    #         category_list.append(category.text)
+    #     category_text = ','.join(category_list)
+    #     # print(category_text)
+    #
+    #     poem_href = href
+    #
+    #     with open(f'data/{key}.csv', 'a', encoding='utf-8-sig') as file:
+    #         row = [value.get('part'), author, author2, poem_name, poem_text_, category_text, poem_href]
+    #         writer = csv.writer(file, delimiter=';')
+    #         writer.writerow(row)
+    #
+    #     print(f'...Обработано: {100 * count / all_count}%')
+    # poem_text =
+    # author2 = get_author_gent(author, morph)
+    # break
+    # count+=1
+    # if count > 3:
+    #     break
+    # row = [
+    #     value.get('part'),
+    #
+    # ]
+    # with open(f'data/{key}.csv', 'a', encoding='utf-8') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow(row)
+
+
+def main():
+    for key, value in DATA.items():
+        get_data(url=value['url'], folder_name=key, part=value['part'])
+
+
+if __name__ == '__main__':
+    main()
